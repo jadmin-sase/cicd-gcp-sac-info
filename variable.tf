6a9c57b502936b826d3c63e3b6ca7a266d995c7b
jadmin-sase <64169796+jadmin-sase@users.noreply.github.com>
2020-05-20 12:25:23 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -61,8 +61,8 @@ variable "tenant_domain" {
 }
 variable "luminate_user" {
   //type        = list(string)
-  //default     = ("joe@SASEDemo.com" "michael.winslow@SASEDemo.com")
-  default = "joe@SASEDemo.com"
+  //default     = ("joe.user@SASEDemo.com" "michael.winslow@SASEDemo.com")
+  default = "joe.user@SASEDemo.com"
   //default = "michael.winslow@SASEDemo.com"
   //default = "mikewinslow@symcmwinslow.luminatesite.com"
 }
